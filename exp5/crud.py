e7bee252c57ce9a5e9db5bd5f424d1d3948816c8
Dusker233 <1187305740@qq.com>
2023-10-28 16:01:10 +0800

    exp5

--- exp5/crud.py
+++ exp5/crud.py
@@ -1,3 +1,4 @@
+# execute `python3 -m doctest -v test.txt` to run the test suite
 from sql_conn import DataBase
 
 db = DataBase('./course.db')
@@ -10,21 +11,56 @@ def retrieve(index):
     """
 
     if index == 0:
-        desp, result = db.Query('select * from courses')
+        desp, results = db.selectAll('courses')
     else:
-        desp, result = db.Query("select * from courses where week = '%s'" % index)
-    assert len(desp) != 0
-    assert result != None
+        desp, results = db.Query2('courses', ['week'], [index])
+    assert desp, results
     print(desp)
-    for course in result:
+    for course in results:
         print(course)
-    return len(result)
+    return len(results)
 
 def create():
-    pass
+    """ create a new course and insert it into the database
+    """
+
+    data = dict(
+        id = 10,
+        week = 6,
+        name = 'pysqlite',
+        duration = '8-8 周',
+        section = '7-8',
+        classroom = '知行南楼 408',
+        teacher = '<NAME>'
+    )
+    db.Insert('courses', data)
+    _, results = db.Query2('courses', ['id'], [10])
+    print(results)
 
 def update():
-    pass
+    """ update the data in the database
+    the type of the ID field is list, which contains the list of id fields
+    rest of the data fields will be updated
+    """
+    _, results = db.Query2('courses', ['id'], [10])
+    print(results)
+    data = dict(
+        ID = ['id'],
+        id = 10,
+        week = 7,
+        section = '9-10'
+    )
+    db.Update('courses', data)
+    _, results = db.Query2('courses', ['id'], [10])
+    print(results)
 
 def delete():
-    pass
+    """ delete the data in the database
+    """
+    _, results = db.Query2('courses', ['id'], [10])
+    print(results)
+    db.DeleteById('courses', 'id', 10)
+    # when id more than 1, use list as parameter
+    # db.DeleteById('courses', ['id'], [10])
+    _, results = db.Query2('courses', ['id'], [10])
+    print(results)
--- exp5/sql_conn.py
+++ exp5/sql_conn.py
@@ -14,7 +14,6 @@ class DataBase:
         return conn
 
     def Query(self, sql):
-        # print(sql)
         conn = self.Open()
         cur = conn.cursor()
         cur.execute(sql)
@@ -28,20 +27,60 @@ class DataBase:
         Close(conn)
         return description, result
 
-    def Update(self, data, table):
+    def selectAll(self, table):
+        conn = self.Open()
+        cur = conn.cursor()
+        sql = "select * from %s" % table
+        # print(sql)
+        cur.execute(sql)
+
+        description = []
+        for d in cur.description:
+            description.append(d[0])
+        result = cur.fetchall()
+
+        cur.close()
+        Close(conn)
+        return description, result
+
+    def Query2(self, table, id, value):
         conn = self.Open()
         cur = conn.cursor()
         values = []
-        idName = list(data)[0]
-        for v in list(data)[1:]:
-            values.append("%s='%s'" % (v, data[v]))
-        sql = "update %s set %s where %s = '%s'" % (table, ",".join(values), idName, data[idName])
+        for i in range(len(id)):
+            values.append("%s='%s'" % (id[i], value[i]))
+        sql = "select * from %s where %s" % (table, ' and '.join(values))
+        # print(sql)
+        cur.execute(sql)
+
+        description = []
+        for d in cur.description:
+            description.append(d[0])
+        result = cur.fetchall()
+
+        cur.close()
+        Close(conn)
+        return description, result
+
+    def Update(self, table, data):
+        conn = self.Open()
+        cur = conn.cursor()
+        values = []
+        ids = []
+        idNames = data['ID']
+        print(idNames)
+        for v in list(data):
+            if v in idNames:
+                ids.append("%s='%s'" % (v, data[v]))
+            elif v != 'ID':
+                values.append("%s='%s'" % (v, data[v]))
+        sql = "update %s set %s where %s" % (table, ",".join(values), " and ".join(ids))
         print(sql)
         cur.execute(sql)
         conn.commit()
         Close(conn)
 
-    def Insert(self, data, table):
+    def Insert(self, table, data):
         conn = self.Open()
         cur = conn.cursor()
         values = []
@@ -54,12 +93,21 @@ class DataBase:
         conn.commit()
         Close(conn)
 
-    def DeleteById(self, id, value, table):
+    def DeleteById(self, table, id, value):
+        # print(type(id))
         conn = self.Open()
         values = []
         cur = conn.cursor()
-        sql = "delete from %s where %s=?" % (table, id)
-        print(sql)
-        cur.execute(sql, (value,))
-        conn.commit()
+        if type(id) in [list]:
+            for i in range(len(id)):
+                values.append("%s='%s'" % (id[i], value[i]))
+            sql = "delete from %s where %s" % (table, " and ".join(values))
+            print(sql)
+            cur.execute(sql)
+            conn.commit()
+        else:
+            sql = "delete from %s where %s=?" % (table, id)
+            print(sql)
+            cur.execute(sql, (value,))
+            conn.commit()
         Close(conn)
